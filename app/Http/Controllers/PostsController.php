fdb20b1a0414e0a61b2921cb7a55f10329f63953
Eslam <eslam.gamal@transglobal.com.eg>
2021-07-05 00:37:48 +0200

    first commit

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
-use App\Models\Post;
+use App\Category;
+use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
